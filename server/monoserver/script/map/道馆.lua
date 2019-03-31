170f393f754b8b6e5991703411bde2dc9c67ad86
etorth <anhonghe@gmail.com>
2019-03-31 06:49:56 +0000

    update script

--- server/monoserver/script/map/道馆.lua
+++ server/monoserver/script/map/道馆.lua
@@ -19,7 +19,7 @@ if g_Inited == nil then
     -- only initialize once
     -- initialize all global/constant variables
 
-    g_MaxMonsterCount = 1
+    g_MaxMonsterCount = 1000
     g_LogicDelay      = 1000
     g_LastInvokeTime  = getTime()
 
@@ -51,7 +51,14 @@ if getTime() - g_LastInvokeTime > g_LogicDelay then
 
     if getMonsterCountInList() < g_MaxMonsterCount then
 
-        addMonster(g_MonsterList[math.random(#g_MonsterList)], 400 + math.random(1, 5), 120 + math.random(1, 5), true)
+        -- add a monster at player location
+        -- addMonster(g_MonsterList[math.random(#g_MonsterList)], 400 + math.random(1, 6), 120 + math.random(1, 4),  true)
+
+        -- randomly add monsters
+        -- don't use rotate_coord, it adds too many monsters at corner
+        for index = 1, 100 do
+            addMonster(g_MonsterList[math.random(#g_MonsterList)], math.random(1, 600), math.random(1, 400), false)
+        end
 
     end
 
